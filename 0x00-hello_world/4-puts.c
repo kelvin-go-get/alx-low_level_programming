7511bdaef7a6cc673120f8f06b7d415427963ef7
kelvin <knjuiri@gmail.com>
2023-09-07 08:35:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - function puts prints
+ * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle\n");
+	 puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
 }

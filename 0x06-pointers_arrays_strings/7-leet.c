c14a3a4a423b9d59053d3c581d2a1d60a3112f46
kelvin <knjuiri@gmail.com>
2023-09-20 09:08:45 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * leet - Encode a string into "1337".
- * @str: The input string to encode.
+ * @str: The input string.
  *
  * Return: A pointer to the modified string.
  */
 char *leet(char *str)
 {
 	char *ptr = str;
-	char *leet_chars = "aAeEoOtTlL";
-	char *leet_replacements = "4433007711";
+	char leet_chars[] = "aAeEoOtTlL";
+	char leet_replacements[] = "4433007711";
+	int i;
 
-	while (*ptr)
+	for (; *ptr; ptr++)
 	{
-	char *leet_ptr = leet_chars;
-	char *replacement_ptr = leet_replacements;
-	int match = 0;
-
-	while (*leet_ptr)
+	for (i = 0; leet_chars[i]; i++)
 	{
-	if (*ptr == *leet_ptr)
+	if (*ptr == leet_chars[i])
 	{
-	*ptr = *replacement_ptr;
-	match = 1;
+	*ptr = leet_replacements[i];
 	break;
 	}
-	leet_ptr++;
-	replacement_ptr++;
-	}
-	if (!match)
-	{
-	ptr++;
 	}
 	}
 	return (str);

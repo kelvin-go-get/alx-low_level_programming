ec54e151d0712ef404b7219c4b41aaaff3fedc1d
kelvin <knjuiri@gmail.com>
2023-09-20 11:05:49 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,39 +2,39 @@
 #include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers.
+ * infinite_add - Add two numbers.
  * @n1: The first number as a string.
  * @n2: The second number as a string.
  * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
+ * @size_r: The size of the buffer r.
  *
- * Return: A pointer to the result, or 0 if the result can't be stored in r.
+ * Return: A pointer to the result or 0 if the result can't be stored in r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, carry = 0, len1 = 0, len2 = 0, max_len, sum;
+	int len1 = 0, len2 = 0, carry = 0, sum, i, j;
 
 	while (n1[len1])
 	len1++;
 	while (n2[len2])
 	len2++;
-	if (len1 >= size_r || len2 >= size_r)
+	if (len1 + 2 > size_r || len2 + 2 > size_r)
 	return (0);
 	i = len1 - 1;
 	j = len2 - 1;
-	max_len = (len1 > len2) ? len1 : len2;
-	r[max_len + 1] = '\0';
+	r[size_r - 1] = '\0';
 	while (i >= 0 || j >= 0 || carry)
 	{
-	int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-	int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-
-	sum = digit1 + digit2 + carry;
+	sum = carry;
+	if (i >= 0)
+	sum += n1[i] - '0';
+	if (j >= 0)
+	sum += n2[j] - '0';
 	carry = sum / 10;
-	r[max_len] = (sum % 10) + '0';
+	r[size_r - 2] = (sum % 10) + '0';
 	i--;
 	j--;
-	max_len--;
+	size_r--;
 	}
-	return (r);
+	return (r + size_r - 1);
 }

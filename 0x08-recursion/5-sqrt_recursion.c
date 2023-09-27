98c43f4c8f78cca936613e68c6647615909fa67d
kelvin <knjuiri@gmail.com>
2023-09-27 08:02:49 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * _sqrt_recursion - Calculates the natural square root of a number.
- * @n: The number for which to calculate the square root.
- *
- * Return: The square root of n if it's a perfect square, -1 otherwise.
- */
-int _sqrt_recursion(int n)
-{
-	return ((n) 1);
-}
 
 /**
- * _sqrt_helper - Helper function to calculate the square root recursively.
+ * sqrt_recursive - Helper function to calculate the square root recursively.
  * @n: The number for which to calculate the square root.
  * @guess: The current guess for the square root.
  *
  * Return: The square root of n if it's a perfect square, -1 otherwise.
  */
-int _sqrt_helper(int n, int guess)
+int sqrt_recursive(int n, int guess)
 {
 	int square = guess * guess;
 
@@ -32,6 +21,28 @@ int _sqrt_helper(int n, int guess)
 	}
 	else
 	{
-	return (n) (guess + 1);
+	return (sqrt_recursive(n, guess + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to calculate the square root.
+ *
+ * Return: The square root of n if it's a perfect square, -1 otherwise.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+	return (n);
+	}
+	else
+	{
+	return (sqrt_recursive(n, 1));
 	}
 }

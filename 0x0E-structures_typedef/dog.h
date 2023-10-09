0bd773873131c7acecef74c06e8e145e88c2497f
kelvin <knjuiri@gmail.com>
2023-10-09 06:24:12 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**struct dog - dog info
+ * @name: name
+ * @age: age
+ * @owner: owns
+ */
 struct dog {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
